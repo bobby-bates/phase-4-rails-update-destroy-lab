0bf39a9b996ac5e2c6fcd76817ca1ae21ce56b64
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-28 06:09:38 -0400

    complete assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  # resources :plants, only: [:index, :show, :create]
+  # Using all the CRUD now:
+  resources :plants
 end
